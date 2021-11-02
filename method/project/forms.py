93c6eef0510c2fb85611b82efb7fafbd0880f530
srh95 <srh95@case.edu>
2021-11-01 22:23:11 -0400

    fixed forms error

--- method/project/forms.py
+++ method/project/forms.py
@@ -7,9 +7,9 @@ class RegisterForm(forms.Form):
     password2 = forms.CharField(label='password*', max_length=50, required=True)
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='username',max_length=50, required=True)
+    username = forms.CharField(label='username', max_length=50, required=True)
     password = forms.CharField(label='Name', max_length=50, required=True)
 
 class TeamForm(forms.Form):
-    teamname = forms.CharField(label = "teamname", max_length = 50, required = True)
-    teamid = forms.NumberInput(required = True)
+    teamname = forms.CharField(label="teamname", max_length=50, required=True)
+    teamid = forms.IntegerField(label="teamid", required=True)
--- method/project/models.py
+++ method/project/models.py
@@ -6,11 +6,13 @@ class User(models.Model):
     user_username = models.CharField(max_length=30)  # username is their email
     user_password = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.user_name
+
 
 class Team(models.Model):
     teamname = models.CharField(max_length=200)
     user_username = models.CharField(max_length=30)
     teamid = models.CharField(max_length=4)
 
-    def __str__(self):
-        return self.user_name
+
