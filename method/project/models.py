8bca252213b097809a05e03a16aa6d6262ebf495
isabellec23 <63928064+isabellec23@users.noreply.github.com>
2021-11-22 16:07:19 -0600

    display teammates

--- method/project/models.py
+++ method/project/models.py
@@ -1,22 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User, Group, AbstractBaseUser
+from django.contrib.auth.models import User, Group
+
 
 class Team(models.Model):
     team_id = models.CharField(max_length=10)
     team_name = models.CharField(max_length=200)
     num_members = models.CharField(max_length=2)
 
+
     def __str__(self):
         return self.team_name
-
-class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    user_username = models.CharField(max_length=30)  # username is their email
-    user_password = models.CharField(max_length=50)
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return self.user_name
 
 class Bracket(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -30,3 +23,18 @@ class Bracket(models.Model):
 
     def __str__(self):
         return self.bracket_name
+
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    user_username = models.CharField(max_length=30)  # username is their email
+    user_password = models.CharField(max_length=50)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user_name
+
+class UserBracketProfiles(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    bracket = models.JSONField(default=list, blank=True, null=True)
+    def __str__(self):
+        return self.user.user_username
